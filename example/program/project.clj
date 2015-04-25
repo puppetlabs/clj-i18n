6a42ebf12a533aac5e72cf9c70288dd044ed90af
David Lutterkort <lutter@watzmann.net>
2015-04-24 18:02:25 -0700

    Keep lein from complaining by adding :main to :aot in project.clj files

--- example/program/project.clj
+++ example/program/project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/i18n/example/program "0.1.0"
+(defproject puppetlabs/i18n-example-program "0.1.0"
   :description "A sample use of the i18n library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [puppetlabs/i18n "0.1.0-SNAPSHOT"]]
   :plugins      [[puppetlabs/i18n "0.1.0-SNAPSHOT"]]
-  :main puppetlabs.i18n.example.program.main)
+  :main puppetlabs.i18n.example.program.main
+  :aot [puppetlabs.i18n.example.program.main])
