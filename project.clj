0648ace5c455111bd35729d23a05de61fb43c3a0
Nick Lewis <nick@puppetlabs.com>
2015-09-09 11:26:58 -0700

    (maint) Add nexus deploy info

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
+(defn deploy-info
+  [url]
+  {:url url
+   :username :env/nexus_jenkins_username
+   :password :env/nexus_jenkins_password
+   :sign-releases false})
+
 (defproject puppetlabs/i18n "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -5,4 +12,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main puppetlabs.i18n.main
-  :aot [puppetlabs.i18n.main])
+  :aot [puppetlabs.i18n.main]
+
+  :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
+                 ["snapshots"  "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
+
+  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
+                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]])
