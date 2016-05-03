2b5870da140ff01936956a5e771ed9c3066c14d4
Jenkins CI <ci@puppetlabs.com>
2016-05-03 08:22:27 -0700

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/i18n "0.3.0-SNAPSHOT"
+(defproject puppetlabs/i18n "0.3.0"
   :description "Clojure i18n library"
   :url "http://github.com/puppetlabs/clj-i18n"
   :license {:name "Apache License, Version 2.0"
