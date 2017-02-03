f22552b0c3f80a81b2cfc2bb08c7bf357225a4aa
libbymo <libbymo@gmail.com>
2017-02-02 17:05:16 -0800

    (INTL-12) Change the POT name from messages to project name.

---
+++ src/leiningen/i18n/utils.clj
@@ -0,0 +1,11 @@
+(ns leiningen.i18n.utils
+  "Plugin for i18n tasks. Start by using i18n init"
+  (:require [clojure.string :as cstr]))
+
+(defn replace-first-in-multiline
+  "Replace first instance of regex-match in the passed in string with the
+   replacement. Uses RegExp's m flag (?m) so that ^/$ matches beginning of line."
+  [s regex-match replacement]
+  (cstr/replace-first s
+                      (re-pattern (str "(?m)^" (.toString regex-match) "$"))
+                      replacement))
